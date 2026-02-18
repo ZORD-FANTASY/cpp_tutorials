92e6e1d7ef02200f0cac415b0b9ca68d85af0247
Fantasy <mk6484988@gmail.com>
2026-02-18 19:30:32 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+    
+using namespace std;
+
+int main() {
+    int score;
+    score = 100;
+
+    const int uid = 12345;
+
+    int ManishCredit = 300;
+    ManishCredit = 500;
+
+    // uid = 65;
+
+    cout << "Welcome to prime of ZORD | FANTASY " << endl ;
+    cout << "Welcome to prime of ZORD | FANTASY " << endl ;
+    cout << "Welcome to prime of ZORD | FANTASY " << endl ;
+    
+    return 0;
+}
