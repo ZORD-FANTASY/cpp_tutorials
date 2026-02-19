c0fece5d95f5be029988ae0f3c99f4760af85824
Fantasy <mk6484988@gmail.com>
2026-02-19 21:36:12 +0530

    Add files via upload

---
+++ data.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+    
+using namespace std;
+
+int main() {
+    int killratios = 50 ;
+    float zordkd = 10.5;
+    double royalpass = 299.99;
+    char gameplaygrade = 'A';
+    bool isitprimezord = false;
+
+    cout << zordkd << endl;
+     
+
+    return 0;
+}
